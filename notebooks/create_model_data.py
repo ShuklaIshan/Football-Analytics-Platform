4ce80725ae3d0a1163313e48ef4c93864126fe7f
Ishan Shukla <shuklaishan077@gmail.com>
2026-06-13 13:59:51 +0530

    A new stats df created

--- notebooks/create_model_data.py
+++ notebooks/create_model_data.py
@@ -1,16 +1,34 @@
 import pandas as pd
 matches = pd.read_csv("data/results.csv")
-team_stats = pd.read_csv("data/home_team_stats.csv")
+home_stats = pd.read_csv("data/home_team_stats.csv")
+away_stats = pd.read_csv("data/away_team_stats.csv")
+
 matches = matches.merge(
-    team_stats,
+    home_stats,
     left_on="home_team",
     right_on="team",
     how="left"
 )
 matches = matches.drop(columns=["team"])
+
+matches = matches.merge(
+    away_stats,
+    left_on="away_team",
+    right_on="team",
+    how="left"
+)
+matches = matches.drop(columns=["team"])
+
 print(matches.head())
 print(
     matches[
-        ["home_win_rate"]
+        [
+        "home_win_rate",
+        "away_win_rate"
+        ]
     ].isnull().sum()
+)
+matches.to_csv(
+    "data/model_data.csv",
+    index=False
 )

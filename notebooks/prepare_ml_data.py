90a732d1d545737a7b33a9ff212c5cc0b5a0b135
Ishan Shukla <shuklaishan077@gmail.com>
2026-06-14 18:04:50 +0530

    New dataframes created as for ml model

---
+++ notebooks/prepare_ml_data.py
@@ -0,0 +1,36 @@
+import pandas as pd
+df = pd.read_csv("data/model_data.csv")
+def get_result(row):
+
+    if row["home_score"] > row["away_score"]:
+        return "Home Win"
+
+    elif row["home_score"] < row["away_score"]:
+        return "Away Win"
+
+    else:
+        return "Draw"
+df["result"] = df.apply(get_result, axis=1)
+print(df["result"].value_counts())
+features = df[
+    [
+        "home_win_rate",
+        "home_draw_rate",
+        "home_loss_rate",
+        "away_win_rate",
+        "away_draw_rate",
+        "away_loss_rate"
+    ]
+]
+target = df["result"]
+print(features.shape)
+print(target.shape)
+features.to_csv(
+    "data/features.csv",
+    index=False
+)
+
+target.to_csv(
+    "data/target.csv",
+    index=False
+)
